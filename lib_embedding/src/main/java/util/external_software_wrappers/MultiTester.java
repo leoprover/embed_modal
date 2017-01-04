c5a00592ddcdf03caaea7ea75c72795c38f445bd
Tobias Gleissner <gleissner.tobias@googlemail.com>
2017-01-04 21:52:27 +0100

    implemented multitester which tests using satallax leo2 and nitpick

--- lib_embedding/src/main/java/util/external_software_wrappers/MultiTester.java
+++ lib_embedding/src/main/java/util/external_software_wrappers/MultiTester.java
@@ -5,6 +5,8 @@ import util.ThfProblem;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -18,11 +20,12 @@ public class MultiTester {
     private static final Logger log = Logger.getLogger( "default" );
     private static final String errorPrefix = "Error_";
 
-    public List<ThfProblem> all;
+    public List<ThfProblem> allProblems;
     private List<String> filterList;
 
     public void testProblemDirectory(Path inPath, Path outPath, long timoutPerProblem, TimeUnit timeUnit, Path filterFile) throws IOException {
 
+        /*
         // remove all old error files
         try(Stream<Path> paths = Files.walk(outPath)){
             paths.filter(f->f.getFileName().toString().contains(errorPrefix)).forEach((path) -> {
@@ -33,6 +36,7 @@ public class MultiTester {
                 }
             });
         }
+        */
 
         // init white filter list
         filterList = null;
@@ -46,6 +50,7 @@ public class MultiTester {
         }
 
         // Test problems
+        allProblems = new ArrayList<>();
         AtomicInteger problems = new AtomicInteger();
         try(Stream<Path> paths = Files.walk(inPath)) {
             paths.filter(Files::isRegularFile)
@@ -56,26 +61,49 @@ public class MultiTester {
                     })
                     .forEach(f -> {
                         problems.incrementAndGet();
+                        //String name = f.getParent().getFileName().toString() + "/" + f.getFileName().toString();
+                        String name = f.getFileName().toString();
                         System.out.println("Processing " + String.valueOf(problems.get()) + " " + f.toString());
                         ThfProblem thfProblem = new ThfProblem(f);
+                        thfProblem.name = name;
+                        allProblems.add(thfProblem);
 
                         // Satallax
                         SatallaxWrapper satallax = new SatallaxWrapper();
                         satallax.call(f, timoutPerProblem, timeUnit);
                         thfProblem.satallax = satallax;
+                        System.out.println(name + ": satallax: " + satallax.getAbbrevStatus());
 
                         // Leo 2
                         Leo2Wrapper leo = new Leo2Wrapper();
                         leo.call(f,timoutPerProblem,timeUnit);
                         thfProblem.leo = leo;
+                        System.out.println(name + ": leo2: " + leo.getAbbrevStatus());
 
                         // Nitpick
                         NitpickWrapper nitpick = new NitpickWrapper();
                         nitpick.call(f,timoutPerProblem,timeUnit);
                         thfProblem.nitpick = nitpick;
+                        System.out.println(name + ": nitpick: " + nitpick.getAbbrevStatus());
                     });
         }
 
+        // Write results to file
+        // subdirectory/filename,satallax_status,satallax_duration,leo_status,leo_duration,nitpick_status,nitpick_duration
+        try {
+            Files.write(Paths.get(outPath.toString(),"all"),this.allProblems.stream()
+                    .map(p->p.name + "," +
+                            p.satallax.getAbbrevStatus() + "," + p.satallax.duration + "," +
+                            p.leo.getAbbrevStatus() + "," + p.leo.duration + "," +
+                            p.nitpick.getAbbrevStatus() + "," + p.nitpick.duration
+                    )
+                    .collect(Collectors.joining("\n")).getBytes());
+        } catch (IOException e) {
+            System.err.println("Could not write all file");
+            e.printStackTrace();
+        }
+
+        // here should come the same stuff as in Problem tester satallax
     }
 
 
