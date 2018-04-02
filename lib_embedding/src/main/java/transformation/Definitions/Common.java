870c20ee9efc6a00cebdd68f869987c499af80f1
TobiasGleissner <gleissner.tobias@googlemail.com>
2018-04-02 22:24:14 +0200

    added normalization for types

--- lib_embedding/src/main/java/transformation/Definitions/Common.java
+++ lib_embedding/src/main/java/transformation/Definitions/Common.java
@@ -1,5 +1,13 @@
 package transformation.Definitions;
 
+import exceptions.ParseException;
+import parser.ParseContext;
+import parser.ThfAstGen;
+import util.tree.Node;
+
+import java.util.Stack;
+
+
 public class Common {
 
     /***************************************************************************
@@ -25,9 +33,44 @@ public class Common {
             "^ [Phi:(" + w + ">$o)] : ( Phi @ mcurrentworld ) ) ) ).";
 
 
-    // TODO normalize parentheses in types
     public static String normalizeType(String type){
-        return type.replaceAll("[ ]","");
+        String t = "";
+        try {
+            ParseContext pc = ThfAstGen.parse(type,"thf_top_level_type","name");
+            Node root = pc.getRoot();
+            Stack<Node> nodes_to_visit = new Stack<>();
+            nodes_to_visit.push(root);
+
+            // remove all parentheses
+            while( !nodes_to_visit.isEmpty() ) {
+                Node current_node = nodes_to_visit.pop();
+                if (current_node.isLeaf() && current_node.getLabel().equals("(")){
+                    current_node.getNextTopBranchingNode().delFirstChild();
+                }
+                if (current_node.isLeaf() && current_node.getLabel().equals(")")){
+                    current_node.getNextTopBranchingNode().delLastChild();
+                }
+                nodes_to_visit.addAll(current_node.getChildren());
+            }
+
+            // add parentheses on every branching node in the parse tree
+            nodes_to_visit = new Stack<>();
+            if (root.hasOneLeaf()) return "(" + root.toStringLeafs() + ")";
+            nodes_to_visit.push(root.getNextBotBranchingNode());
+            while( !nodes_to_visit.isEmpty() ) {
+                Node current_node = nodes_to_visit.pop();
+                for (Node child : current_node.getChildren()){
+                    Node next = child.getNextBotBranchingNode();
+                    if (next != null) nodes_to_visit.add(next);
+                }
+                current_node.addChildAt(new Node("(","("),0);
+                current_node.addChild(new Node(")",")"));
+            }
+            t = root.toStringLeafs();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return t;
     }
 
     // THIS IS UGLY - replace it by numbering quantifications later
