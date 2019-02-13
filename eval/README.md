c6a4ee113e91d6c1dcd6aad0207970a69b3ad662
Tobias Gleißner <tobias.gleissner@fu-berlin.de>
2019-02-13 14:50:28 +0100

    added run functions for evaluation

---
+++ eval/README.md
@@ -0,0 +1,8 @@
+# Files
+* `common.py` contains the **database configuration** and
+  the **run configurations** that should be tested
+* `create_tables.py` contains a method to create a fresh database table 
+  for the db/run configurations in config
+* `extract_qmltp_info.py` can extract the SZS status information from a QMLTP file 
+  that was converted to THF with modal extension
+* `run_prover` runs the MET for all run configurations
---
+++ eval/extract_qmltp_info.py
@@ -0,0 +1,47 @@
+import os
+import common
+from pathlib import *
+
+def extract_qmltp_info_from_problem(problem):
+    start = problem.find("% Status") + len("% Status")
+    end = problem.find("% Rating")
+    info = problem[start:end]\
+        .replace(":","")\
+        .replace("varying","")\
+        .replace("cumulative","")\
+        .replace("constant","")\
+        .replace("v1.1","")\
+        .replace("K ","  ")\
+        .replace("D ","  ")\
+        .replace("T ","  ")\
+        .replace("S4 ","   ")\
+        .replace("S5 ","   ")\
+        .replace("%","")\
+        .strip()\
+        .split("\n")
+    info = list(map(lambda e: e.strip(),info))
+    info = list(map(lambda e:
+                    list(filter(lambda x: len(x)!=0,e.split(" "))),info))
+    return info
+
+# filename, prover, status, wc_time, cpu_time, system, quantification, consequence, constants, transformation_parameter_list
+def extract_qmltp_info_from_problem_file_list(problem_file_list):
+    res = []
+    for p in problem_file_list:
+        with open(p,"r") as fh:
+            content = fh.read()
+            info = extract_qmltp_info_from_problem(content)
+            for system, s_val in zip(("$modal_system_K","$modal_system_D","$modal_system_T","$modal_system_S4","$modal_system_S5"),info):
+                for quantification, q_val in zip(("$varying","$cumulative","$constant"),s_val):
+                    line = [p.name, "QMLTP", q_val, "0", "0",
+                              system, quantification, "$local", "$rigid",
+                              ""]
+                    res.append(line)
+    return res
+
+
+
+line_list = extract_qmltp_info_from_problem_file_list(common.get_problem_file_list(common.problem_directory))
+print(line_list)
+
+
