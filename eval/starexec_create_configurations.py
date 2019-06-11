79b9f852c141e24bb2b989d5b619296741c10fd6
Tobias Gleißner <tobias.gleissner@fu-berlin.de>
2019-06-11 10:03:48 +0200

    added const_syn to starexec conf

--- eval/starexec_create_configurations.py
+++ eval/starexec_create_configurations.py
@@ -10,7 +10,7 @@ class ProverInstance(enum.Enum):
     SATALLAX_E_PICOMUS = "../satallax -M ../modes -E ../externals/eprover -P ../externals/picomus -t $STAREXEC_CPU_LIMIT embeddedProblem"
     HOLYHAMMER         = ""
 
-def createStarExecBinConfiguration(prover:ProverInstance,consequence,constants,quantification,system,param_cumu_quant,param_decr_quant,param_mod):
+def createStarExecBinConfiguration(prover:ProverInstance,consequence,constants,quantification,system,param_cumu_quant,param_decr_quant,param_const_quant,param_mod):
     sb = []
     sb.append("#!/bin/bash")
     sb.append("")
@@ -23,26 +23,27 @@ def createStarExecBinConfiguration(prover:ProverInstance,consequence,constants,q
     sb.append("SYSTEMS=\"" + system + "\"")
     sb.append("PARAM_CUMU_QUANT=\"" + param_cumu_quant + "\"")
     sb.append("PARAM_DECR_QUANT=\"" + param_decr_quant + "\"")
+    sb.append("PARAM_CONST_QUANT=\"" + param_const_quant + "\"")
     sb.append("PARAM_MOD=\"" + param_mod + "\"")
     sb.append("")
-    sb.append("java -Xss128m -Xmx2g -Xms1g -jar ../embed.jar -i $1 -o embeddedProblem -consequences $CONSEQUENCES -constants $CONSTANTS -domains $DOMAINS -systems $SYSTEMS -t $PARAM_DECR_QUANT,$PARAM_CUMU_QUANT,$PARAM_MOD")
+    sb.append("java -Xss128m -Xmx2g -Xms1g -jar ../embed.jar -i $1 -o embeddedProblem -consequences $CONSEQUENCES -constants $CONSTANTS -domains $DOMAINS -systems $SYSTEMS -t $PARAM_DECR_QUANT,$PARAM_CUMU_QUANT,$PARAM_CONST_QUANT,$PARAM_MOD")
     sb.append("")
     sb.append(prover.value)
     sb.append("")
     return "\n".join(sb)
 
 def get_transformation_abbreviation(transformation_param):
-    if transformation_param == "semantic_monotonic_quantification":
+    if transformation_param == "semantic_cumulative_quantification":
         return "semcumul"
-    if transformation_param == "semantic_antimonotonic_quantification":
+    if transformation_param == "semantic_decreasing_quantification":
         return "semdecr"
     if transformation_param == "semantic_modality_axiomatization":
         return "semmod"
     if transformation_param == "semantic_constant_quantification":
         return "semconst"
-    if transformation_param == "syntactic_monotonic_quantification":
+    if transformation_param == "syntactic_cumulative_quantification":
         return "syncumul"
-    if transformation_param == "syntactic_antimonotonic_quantification":
+    if transformation_param == "syntactic_decreasing_quantification":
         return "syndecr"
     if transformation_param == "syntactic_modality_axiomatization":
         return "synmod"
@@ -51,7 +52,7 @@ def get_transformation_abbreviation(transformation_param):
 
 def createConfigurations(outPath:Path, prover:ProverInstance,
         consequence_list, constants_list, quantification_list, system_list,
-        transformation_param_cumu_quant,transformation_param_decr_quant,transformation_param_mod):
+        transformation_param_cumu_quant,transformation_param_decr_quant,transformation_param_const_quant,transformation_param_mod):
     for consequence_property in consequence_list:
         for constant_property in constants_list:
             for quantification_property in quantification_list:
@@ -63,6 +64,7 @@ def createConfigurations(outPath:Path, prover:ProverInstance,
                                                                    system_property,
                                                                    transformation_param_cumu_quant,
                                                                    transformation_param_decr_quant,
+                                                                   transformation_param_const_quant,
                                                                    transformation_param_mod)
                     configuration_name = "_".join([consequence_property,
                                                    constant_property,
@@ -70,6 +72,7 @@ def createConfigurations(outPath:Path, prover:ProverInstance,
                                                    system_property,
                                                    get_transformation_abbreviation(transformation_param_cumu_quant),
                                                    get_transformation_abbreviation(transformation_param_decr_quant),
+                                                   get_transformation_abbreviation(transformation_param_const_quant),
                                                    get_transformation_abbreviation(transformation_param_mod)])
                     configuration_filename = "starexec_run_" + configuration_name
                     out_file = outPath / configuration_filename
@@ -87,8 +90,8 @@ def createAllConfigurations(outPath:Path,prover:ProverInstance):
     quantification_list_list = [
         ["constant","varying","cumulative","decreasing"],
         ["constant","varying","cumulative","decreasing"],
-        ["cumulative","decreasing"],
-        ["cumulative","decreasing"]
+        ["constant","cumulative","decreasing"],
+        ["constant","cumulative","decreasing"]
     ]
     system_list_list = [
         ["K","D","T","S4","S5","S5U"],
@@ -97,16 +100,22 @@ def createAllConfigurations(outPath:Path,prover:ProverInstance):
         ["K","D","T","S4","S5","S5U"]
     ]
     transformation_param_cumu_quant_list = [
-        "semantic_antimonotonic_quantification",
-        "semantic_antimonotonic_quantification",
-        "syntactic_antimonotonic_quantification",
-        "syntactic_antimonotonic_quantification"
+        "semantic_cumulative_quantification",
+        "semantic_cumulative_quantification",
+        "syntactic_cumulative_quantification",
+        "syntactic_cumulative_quantification"
     ]
     transformation_param_decr_quant_list = [
-        "semantic_monotonic_quantification",
-        "semantic_monotonic_quantification",
-        "syntactic_monotonic_quantification",
-        "syntactic_monotonic_quantification"
+        "semantic_decreasing_quantification",
+        "semantic_decreasing_quantification",
+        "syntactic_decreasing_quantification",
+        "syntactic_decreasing_quantification"
+    ]
+    transformation_param_const_quant_list = [
+        "semantic_constant_quantification",
+        "semantic_constant_quantification",
+        "syntactic_constant_quantification",
+        "syntactic_constant_quantification"
     ]
     transformation_param_mod_list = [
         "semantic_modality_axiomatization",
@@ -116,26 +125,26 @@ def createAllConfigurations(outPath:Path,prover:ProverInstance):
     ]
     enable_configuration = [
         True,
+        False,
         True,
-        True,
-        True
+        False
     ]
     for enabled, \
         consequence_list, constants_list, quantification_list, system_list, \
-        transformation_param_cumu_quant, transformation_param_decr_quant, \
+        transformation_param_cumu_quant, transformation_param_decr_quant, transformation_param_const_quant, \
         transformation_param_mod \
         in zip(enable_configuration, \
                consequence_list_list,constants_list_list,quantification_list_list,system_list_list, \
-               transformation_param_cumu_quant_list,transformation_param_decr_quant_list, \
+               transformation_param_cumu_quant_list,transformation_param_decr_quant_list, transformation_param_const_quant_list, \
                transformation_param_mod_list):
         if enabled:
             createConfigurations(outPath,prover, \
                                  consequence_list,constants_list,quantification_list,system_list, \
-                                 transformation_param_cumu_quant,transformation_param_decr_quant, \
+                                 transformation_param_cumu_quant,transformation_param_decr_quant, transformation_param_const_quant, \
                                  transformation_param_mod)
 
 def main(out_dir):
-    prover = ProverInstance.SATALLAX_E_PICOMUS
+    prover = ProverInstance.NITPICK
     outPath = Path(out_dir)
     outPath.mkdir(exist_ok=True)
     createAllConfigurations(outPath,prover)
